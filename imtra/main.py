4052cb3737c07b28fa58d8967e9febe87ede0ce5
Ondrej Levinsky <levinsky.ondrej@gmail.com>
2025-08-21 18:02:09 +0200

    refactor: remove commented legacy logging code

--- imtra/main.py
+++ imtra/main.py
@@ -3,14 +3,8 @@ import sys
 from imtra.io_handling import select_file, buffer_file, select_transformation, write_buffer, parse_arguments
 
 def main():
-#    logging.basicConfig(
-#        level = logging.DEBUG,
-#        stream = sys.stdout,
-#        format = '%(asctime)s - %(levelname)s - %(message)s'
-#    )
 
     if len(sys.argv) == 1:
-#        logging.info("NO ARGUMENTS PROVIDED") # launch interactive mode
         
         input("Interactive Mode Initialized. Press Enter to select a file.")
         
@@ -27,7 +21,6 @@ def main():
         return 0
 
     else:
-#        logging.info("SOME ARGUMENTS PROVIDED") # parse and handle input
 
         try:
             source_path, transformation_stack, write_path = parse_arguments()
